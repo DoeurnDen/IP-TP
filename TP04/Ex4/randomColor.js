644c94702417dba6321b6e5521c90b3bfe40d839
doeurnden <doeurnden32@gmail.com>
2023-02-15 01:03:06 +0700

    update code

--- TP04/Ex4/randomColor.js
+++ TP04/Ex4/randomColor.js
@@ -1,26 +1,21 @@
-let generate = document.getElementById('generate');
-
 class Color{
     colorType = ''
-    records = []//aray
-    
+    records = []
+
     constructor(type){
         this.colorType = type;
     }
+
     generate(){
-        const randomColor = 
-        Math.floor(Math.random()*16777215).toString(16);
+
     }
     getRandomNumber(){
-        
+        return Math.random();
     }
     getRecords(){
-        
+
     }
     reset(){
-        
+
     }
 }
-let col = new Color();
-
-generate.addEventListener("click", col.generate());
