7b1333fa97f525d69b42e0eedfdd7fe5bf93084a
doeurnden <doeurnden32@gmail.com>
2023-02-26 03:41:27 +0700

    add data

---
+++ TP05/Ex5/bookStore.js
@@ -0,0 +1,30 @@
+
+const url = 'https://jsonplaceholder.typicode.com/photos'
+
+const getData=()=>{
+    fetch(url).then(res=>res.json()).then(data=>{
+        render(data)
+    })
+}
+const render = (data)=>{
+    let tmp = "";
+    tmp.forEach(item => {
+        tmp += `
+            <div class="box">
+                <div>
+                    <img src="${item.url} alt="${item.albumId}-${item.title}">
+                </div>
+                <div>
+                    <p>${item.title}</p>
+                    <p>Album Id${item.albumId}</p>
+                    <p>Category: ${item.id}</p>
+                    <p>
+                        <a href="/see.html?id=${item.id}">see</a>
+                    </p>
+                </div>
+            </div>
+        `
+        document.querySelector('.output-data').innerHTML = tmp
+    })
+    window.addEventListener('load',getData());
+}
